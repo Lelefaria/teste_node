3ed73e41561a845f5895bf94011452def704de71
Leanderson Nascimeto <leandersonfaria11@hotmail.com>
2023-12-26 15:07:33 -0300

    fatch: Novas mudancas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,15 +3,15 @@ import { Request, Response, Router } from "express";
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.send('ola mundo ')
+  res.render('home')
 })
 
 router.get('/sobre', (req: Request, res: Response) => {
   res.send('Pagina Institucional ')
 })
 
-router.get('/conato', (req: Request, res: Response) => {
+router.get('/contatos', (req: Request, res: Response) => {
   res.send('instagram e facebook ')
 })
 
-export default Router
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,24 @@
-import express from "express"
-import mainRoutes from './routes/index'
+import express, { Request, Response } from "express";
+import path, { dirname } from "path";
+import mainRoutes from './routes/index';
+import mustache from 'mustache-express';
 
-const server = express()
+const server = express();
 
-server.use(mainRoutes)
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname,'views'));
+server.engine('mustache',mustache());
 
-server.listen(3000,()=>{
-console.log("server rodando")
-})
+server.use(express.static(path.join(__dirname,'../public')));
+server.use(mainRoutes);
+
+server.use((req: Request, res: Response) => {
+  res.status(404).send('Página nao encontrada!')
+});
+
+server.listen(3000, () => {
+  console.log("server rodando")
+});
 
 
 
